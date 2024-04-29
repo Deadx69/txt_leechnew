7ac935b74f1e9290214798e6afc2b4de5644568a
Deadx69 <160730258+Deadx69@users.noreply.github.com>
2024-04-29 09:29:03 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 eapi_id = "23421759"
 api_hash = "ba8ae5b1f703fc771d1e8425db8752d1"
-bot_token = "7092334965:AAErP4tkv5Cy8fRnTeC03_sdmPNGueNmjUg"
+bot_token = "6832001724:AAGWu0pPbK7eC3Fyt8bLuaJiw6lSE6Bahdc"
